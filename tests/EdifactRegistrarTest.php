a7779629e3508280b9bd5a71b12c40f4cfdec57e
Nils Refle <n.refle@gmail.com>
2016-05-20 12:22:55 +0200

    Test vervollständigt

--- tests/EdifactRegistrarTest.php
+++ tests/EdifactRegistrarTest.php
@@ -8,36 +8,47 @@ use Proengeno\Edifact\Exceptions\ValidationException;
 
 class EdifactRegistrarTest extends TestCase 
 {
+    public function tearDown()
+    {
+        EdifactRegistrar::addMessage('ZZZ', null);
+        EdifactRegistrar::addSegement('ZZZ', null);
+    }
+    
     /** @test */
-    public function it_can_regsiter_resolve_a_new_segment_class_path()
+    public function it_can_regsiter_and_resolve_a_new_segment_class_path()
     {
         $pathToSegementClass = Segment::class;
 
         EdifactRegistrar::addSegement('ZZZ', $pathToSegementClass);
 
         $this->assertEquals($pathToSegementClass, EdifactRegistrar::getSegment('ZZZ'));
-
-        EdifactRegistrar::addSegement('ZZZ', null);
     }
 
     /** @test */
-    public function it_can_regsiter_resolve_a_new_message_class_path()
+    public function it_can_regsiter_and_resolve_a_new_message_class_path()
     {
         $pathToMessageClass = Message::class;
 
-        EdifactRegistrar::addMessage('UTILMD', $pathToMessageClass);
+        EdifactRegistrar::addMessage('ZZZ', $pathToMessageClass);
 
-        $this->assertEquals($pathToMessageClass, EdifactRegistrar::getMessage('UTILMD'));
-
-        EdifactRegistrar::addMessage('ZZZ', null);
+        $this->assertEquals($pathToMessageClass, EdifactRegistrar::getMessage('ZZZ'));
     }
 
     /** @test */
-    public function it_throws_an_exception_if_the_class_is_not_registered()
+    public function it_throws_an_exception_if_the_segment_is_not_registered()
     {
         $pathToSegementClass = Segment::class;
         
         $this->expectException(ValidationException::class);
         $this->assertEquals($pathToSegementClass, EdifactRegistrar::getSegment('ZZZ'));
+    }
+
+    /** @test */
+    public function it_throws_an_exception_if_the_message_is_not_registered()
+    {
+        $pathToSegementClass = Segment::class;
+        
+        $this->expectException(ValidationException::class);
+        $this->assertEquals($pathToSegementClass, EdifactRegistrar::getMessage('ZZZ'));
     }
 }
