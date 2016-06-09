fd2c180bfbc2692c876db931712bc8e29e77cd89
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-06-09 18:00:59 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Interfaces/SegInterface.php
+++ src/Interfaces/SegInterface.php
@@ -5,10 +5,10 @@ namespace Proengeno\Edifact\Interfaces;
 use Proengeno\Edifact\Message\Delimiter;
 
 interface SegInterface {
-	public static function fromSegLine($segLine);
+    public static function fromSegLine($segLine);
     public static function setDelimiter(Delimiter $delimiter);
     public static function getDelimiter();
-	public function name();
-	public function validate();
-	public function __toString();
+    public function name();
+    public function validate();
+    public function __toString();
 } 
